e21cd183aaddc0b92fe6b73068c4df57f03a5c60
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2020-04-04 09:39:40 +0700

    Add KnifeBehavior.java class

---
+++ src/com/erikriosetiawan/KnifeBehavior.java
@@ -0,0 +1,9 @@
+package com.erikriosetiawan;
+
+public class KnifeBehavior implements WeaponBehavior {
+
+    @Override
+    public void useWeapon() {
+        System.out.println("Attack with knife");
+    }
+}
