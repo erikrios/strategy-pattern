d4ed9ab10e3f51fbc93bebebca682fa90c9ed236
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2020-04-04 10:08:26 +0700

    Add Troll.java class

--- src/com/erikriosetiawan/client/Troll.java
+++ src/com/erikriosetiawan/client/Troll.java
@@ -1,4 +1,15 @@
 package com.erikriosetiawan.client;
 
-public class Troll {
+import com.erikriosetiawan.encapsulatedweaponbehavior.AxeBehavior;
+
+public class Troll extends Character {
+
+    public Troll() {
+        weapon = new AxeBehavior();
+    }
+
+    @Override
+    void fight() {
+        System.out.println("The Troll is fight now");
+    }
 }
