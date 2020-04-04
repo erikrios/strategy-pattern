85750af98ed9cdfa8b7c7bf58146cc33ca119982
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2020-04-04 10:02:02 +0700

    Add King.java class

--- src/com/erikriosetiawan/client/King.java
+++ src/com/erikriosetiawan/client/King.java
@@ -1,8 +1,15 @@
 package com.erikriosetiawan.client;
 
+import com.erikriosetiawan.encapsulatedweaponbehavior.SwordBehavior;
+
 public class King extends Character {
 
     public King() {
+        weapon = new SwordBehavior();
+    }
 
+    @Override
+    void fight() {
+        System.out.println("The king is fight now");
     }
 }
