740fae842a9f4e51114fddeaad7805b6dddf3e6f
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2020-04-04 09:41:02 +0700

    Add BowAndArrowBehavior.java class

---
+++ src/com/erikriosetiawan/BowAndArrowBehavior.java
@@ -0,0 +1,9 @@
+package com.erikriosetiawan;
+
+public class BowAndArrowBehavior implements WeaponBehavior{
+
+    @Override
+    public void useWeapon() {
+        System.out.println("Attack with Bow and Arrow");
+    }
+}
