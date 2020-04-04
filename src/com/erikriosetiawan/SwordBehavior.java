053c3377570b4808bebdee8551b550cdc37b4416
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2020-04-04 09:43:14 +0700

    Add SwordBehavior.java class

---
+++ src/com/erikriosetiawan/SwordBehavior.java
@@ -0,0 +1,8 @@
+package com.erikriosetiawan;
+
+public class SwordBehavior implements WeaponBehavior{
+    @Override
+    public void useWeapon() {
+        System.out.println("Attack with Sword");
+    }
+}
