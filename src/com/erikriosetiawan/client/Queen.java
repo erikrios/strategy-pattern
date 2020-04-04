44d38022c0f2100a930be03773943f0d5ec6127e
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2020-04-04 10:07:17 +0700

    Add Queen.java class

--- src/com/erikriosetiawan/client/Queen.java
+++ src/com/erikriosetiawan/client/Queen.java
@@ -1,4 +1,15 @@
 package com.erikriosetiawan.client;
 
-public class Queen {
+import com.erikriosetiawan.encapsulatedweaponbehavior.KnifeBehavior;
+
+public class Queen extends Character {
+
+    public Queen() {
+        weapon = new KnifeBehavior();
+    }
+
+    @Override
+    void fight() {
+        System.out.println("The Queen is fight now");
+    }
 }
