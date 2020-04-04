92b6741b63bc268b3f1176a1a298ae89f05aa6d1
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2020-04-04 09:42:02 +0700

    Add AxeBehavior.java class

---
+++ src/com/erikriosetiawan/AxeBehavior.java
@@ -0,0 +1,9 @@
+package com.erikriosetiawan;
+
+public class AxeBehavior implements WeaponBehavior {
+
+    @Override
+    public void useWeapon() {
+        System.out.println("Attack with Axe");
+    }
+}
