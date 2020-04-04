38f9b0fa5524563be6c6b92e9a5a1db819193c12
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2020-04-04 09:45:09 +0700

    Add weapon field

--- src/com/erikriosetiawan/Character.java
+++ src/com/erikriosetiawan/Character.java
@@ -2,7 +2,7 @@ package com.erikriosetiawan;
 
 public class Character {
 
-    WeaponBehavior weaponBehavior;
+    WeaponBehavior weapon;
 
     public void fight() {
         System.out.println("The character is fight");
